1525cea9ff7af7744767426617c3364252109783
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-16 15:48:06 +0100

    Implementata inversione stringa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function invertiStringa(stringaDaInvertire) {
+    stringaDaInvertire = stringaDaInvertire.trim();
+    let risultatoInversione = '';
+
+    for (let i = stringaDaInvertire.length - 1; i >= 0; i--) {
+        const carattere = stringaDaInvertire[i];
+        risultatoInversione += carattere;
+    }
+
+    return risultatoInversione;
+}
+
+console.log(invertiStringa("Ciao a tutti!"));
