42c42db64882e306d7b3706bcb120194e1dc03c5
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-16 17:06:02 +0100

    Iniziata creazione per il secondo esercizio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 "use strict";
 
-const parolaDaTestare = prompt("Inserisci una parola per vedere se è palindroma: ");
-if (isPalindrome(parolaDaTestare)) {
-    alert("La parola inserita è palindroma!");
-} else {
-    alert("La parola inserita non è palindroma...");
+// const parolaDaTestare = prompt("Inserisci una parola per vedere se è palindroma: ");
+// if (isPalindrome(parolaDaTestare)) {
+//     alert("La parola inserita è palindroma!");
+// } else {
+//     alert("La parola inserita non è palindroma...");
+// }
+
+const scelte = [
+    "Pari",
+    "Dispari"
+];
+
+let sceltaUtente = isScelta(scelte, prompt(`Scegli un'opzione dalle seguenti:
+${assemblaStringaScelta(scelte)}`));
+while (sceltaUtente == -1) {
+    sceltaUtente = isScelta(scelte, prompt(`Quella non è un'opzione!
+Scegli un'opzione dalle seguenti:
+${assemblaStringaScelta(scelte)}`));
 }
+
+sceltaUtente = scelte[sceltaUtente].toLowerCase();
+
+
 
 function invertiStringa(stringaDaInvertire) {
     stringaDaInvertire = stringaDaInvertire.trim();
@@ -27,3 +44,32 @@ function isPalindrome(stringa) {
     return false;
 }
 
+
+function isScelta(arrayDiScelte, scelta) {
+    // Se la scelta è valida, ritorniamo l'index di quella scelta, altrimenti -1
+    const parsedScelta = parseInt(scelta);
+    if (!parsedScelta || parsedScelta < 0) {
+        return -1;
+    }
+
+    if (arrayDiScelte[parsedScelta - 1]) {
+        return parsedScelta - 1;
+    }
+
+    for (let i = 0; i < arrayDiScelte.length; i++) {
+        const valoreArray = arrayDiScelte[i - 1];
+        if (valoreArray === scelta) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function assemblaStringaScelta(arrayDiScelte) {
+    let stringaFinale = "";
+    for (let i = 0; i < arrayDiScelte.length; i++) {
+        const valoreDellArray = arrayDiScelte[i];
+        stringaFinale += `${i + 1}. ${valoreDellArray}\n`;
+    }
+    return stringaFinale;
+}
